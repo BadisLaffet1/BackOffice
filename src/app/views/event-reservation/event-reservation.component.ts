d9d8aa47acc3b88bec490ddc98f24ccdc7b54dce
badisairways@yahoo.fr <badisairways@yahoo.fr>
2023-04-26 06:29:49 +0200

    msg

---
+++ src/app/views/event-reservation/event-reservation.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-reservation',
+  templateUrl: './event-reservation.component.html',
+  styleUrls: ['./event-reservation.component.scss']
+})
+export class EventReservationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
